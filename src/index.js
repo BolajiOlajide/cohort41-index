65ed86800d124f8ae4a58f9e5147edf20635757b
Bolaji Olajide <bolaji.olajide@andela.com>
2018-10-17 16:02:15 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Main = () => (
+    <h1>It's working</h1>
+);
+
+render(<Main />, document.getElementById('app'));
---
+++ src/user.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class User extends React.Component {
+    render() {
+        return (
+            <p>Yeah</p>
+        )
+    }
+}
+
+export default User;
